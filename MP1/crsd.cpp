f6306e2c5f169ecc5c86d1a2329f24fc9c142d3a
Lane Dibello <lane.dibello@gmail.com>
2022-01-30 21:02:34 -0600

    almost there...

--- MP1/crsd.cpp
+++ MP1/crsd.cpp
@@ -22,16 +22,33 @@ void* command_server(void* clientfd);
 void* chat_server(void* port);
 void* cs_daemon(void* csi);
 void* bs_daemon(void* cri);
+void* deletion_awaiter(void *);
 void notify_delete(int sig);
+void join_all(std::vector<pthread_t> ts);
+void enqueue_msg(std::string msg, std::queue<std::string>* mq, pthread_mutex_t* mql, int fd);
 
-struct cs_info{
+struct cs_info{ //Chat Server info
     int fd;
     std::queue<std::string>* msg_queue;
     pthread_mutex_t* ms_lock;
     std::vector<int>* fd_pool;
 };
 
-void *chat_server(void* port){
+struct cd_info{ //Chat dispatcher info
+    char* port;
+    bool* terminated;
+};
+
+struct d_info{ //deletion info
+    cd_info* cdi;
+    std::queue<std::string>* msq;
+    pthread_mutex_t* msqlock;
+    std::vector<pthread_t>* tids;
+    pthread_t u;
+    pthread_t parent;
+};
+
+void *chat_server(void* cdi){
     //Create a server socket and do all the bindings etc
     int rv;
     int sockfd;
@@ -42,7 +59,7 @@ void *chat_server(void* port){
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    if ((rv = getaddrinfo(NULL, (char*)port, &hints, &serv)) != 0) {
+    if ((rv = getaddrinfo(NULL, ((cd_info*)cdi)->port, &hints, &serv)) != 0) {
         perror("getaddrinfo for Chat server failed");
         pthread_exit(NULL);
     }
@@ -74,7 +91,21 @@ void *chat_server(void* port){
     //Create Broadcast thread that will send to these fds when a msg is queued
     pthread_t u;
     pthread_create(&u, NULL, bs_daemon, cri);
-    pthread_detach(u);
+    //pthread_detach(u);
+
+    //List of thread IDs to join when it's time to delete this server
+    std::vector<pthread_t>* tids = new std::vector<pthread_t>;
+    
+    pthread_t dt;
+    d_info* di = new d_info;
+    di->cdi = (cd_info*)cdi;
+    di->msq = msq;
+    di->msqlock = msqlock;
+    di->tids = tids;
+    di->u = u;
+    di->parent = pthread_self();
+    pthread_create(&dt, NULL, deletion_awaiter, (void*)di);
+    pthread_detach(dt);
 
     //accept incoming traffic
     while(1){
@@ -90,15 +121,38 @@ void *chat_server(void* port){
         rfds->push_back(chatfd);
         pthread_t t;
         pthread_create(&t, NULL, cs_daemon, csi);
-        pthread_detach(t);
+        tids->push_back(t);
+        //pthread_detach(t);
     }
 
+}
+
+void* deletion_awaiter(void* di){
+    //Terminated check
+    cd_info* cdi = ((d_info*)di)->cdi;
+    std::queue<std::string>* msq = ((d_info*)di)->msq;
+    pthread_mutex_t* msqlock = ((d_info*)di)->msqlock;
+    std::vector<pthread_t>* tids = ((d_info*)di)->tids;
+    pthread_t u = ((d_info*)di)->u;
+    pthread_t parent = ((d_info*)di)->parent;
+    while(1){
+        if (cdi->terminated){
+            enqueue_msg("chat room being deleted, shutting down connection...", msq, msqlock, -1);
+            sleep(1); //wait for IO
+            for (int i = 0; i < tids->size(); i++){
+                pthread_cancel(tids->at(i));
+            }
+            pthread_join(u, NULL);
+            pthread_cancel(parent);
+            return NULL;
+        }
+    }
     
 }
 
-void enqueue_msg(std::string msg, std::queue<std::string>* mq, pthread_mutex_t* mql){
+void enqueue_msg(std::string msg, std::queue<std::string>* mq, pthread_mutex_t* mql, int fd){
     pthread_mutex_lock(mql);
-    mq->push(msg);
+    mq->push(msg + "\n" + std::to_string(fd));
     pthread_mutex_unlock(mql);
 }
 
@@ -119,7 +173,7 @@ void* cs_daemon(void* csi){
         //broadcast the message out to every client
         std::string msg(msgbuff);
         if(msg.length() < 1) continue;
-        enqueue_msg(msg, ((cs_info*)csi)->msg_queue, ((cs_info*)csi)->ms_lock);
+        enqueue_msg(msg, ((cs_info*)csi)->msg_queue, ((cs_info*)csi)->ms_lock, ((cs_info*)csi)->fd);
     }
 }
 
@@ -128,9 +182,18 @@ void* bs_daemon(void* cri){
     while(1){
         while(((cs_info*)cri)->msg_queue->empty()) usleep(20000); //20 ms ping
         std::string msg = dequeue_msg(((cs_info*)cri)->msg_queue, ((cs_info*)cri)->ms_lock);
-        if(msg.length() < 1) continue;
+        if(msg.find('\n') == 0) continue;
+        std::string rmsg = msg.substr(0, msg.find('\n'));
+        int skip_fd = atoi(msg.substr(msg.find('\n') + 1).c_str());
         for (int i = 0; i < ((cs_info*)cri)->fd_pool->size(); i++){
-            retmsg(((cs_info*)cri)->fd_pool->at(i), (void*)msg.c_str(), msg.length() + 1);
+            if ((((cs_info*)cri)->fd_pool->at(i) == skip_fd)) continue;
+            retmsg(((cs_info*)cri)->fd_pool->at(i), (void*)rmsg.c_str(), msg.length() + 1);
+        }
+        if (skip_fd = -1){
+            for (int i = 0; i < ((cs_info*)cri)->fd_pool->size(); i++){
+                close(((cs_info*)cri)->fd_pool->at(i));
+            }
+            pthread_exit(NULL);
         }
     }
 }
@@ -142,6 +205,7 @@ class DB {
         int port;
         pthread_t tid;
         size_t member_count;
+        bool* term;
     };
     
     short lastport = 1024;
@@ -169,11 +233,16 @@ class DB {
         std::string lport = std::to_string(lastport);
         char * hlport = new char [lport.length()+1];
         strcpy(hlport, lport.c_str());
-        pthread_create(&cst, NULL, chat_server, (void*)hlport);
-        pthread_detach(cst);
+        bool* term = new bool(false);
+        cd_info* cdi = new cd_info;
+        cdi->port = hlport;
+        cdi->terminated = term;
+        pthread_create(&cst, NULL, chat_server, (void*)cdi);
+        //pthread_detach(cst);
         
         lastport++;
         se.tid = cst;
+        se.term = term;
         chatDB.push_back(se);
         
         return se.port;
@@ -197,7 +266,9 @@ class DB {
         for (size_t i = 0; i < chatDB.size(); i++)
         {
             if (chatDB.at(i).name == chat_name){
-                pthread_cancel(chatDB.at(i).tid);
+                //pthread_cancel(chatDB.at(i).tid);
+                *chatDB.at(i).term = true;
+                pthread_join(chatDB.at(i).tid, NULL);
                 chatDB.erase(chatDB.begin() + i);
                 return true;
             }
